ac2ec853ce8fa11d4d81f1a8afd6c318f85ab223
Matheus Cavalcante <matheuscavalcs@gmail.com>
2023-03-22 20:31:30 -0300

    database, models and controllers

---
+++ database/migrations/2023_03_22_220346_create_reports_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+
+            //APARECE
+            $table->string("title");
+            $table->foreignId('user_id')->constrained();
+            $table->string("city");
+            $table->string("description");
+            $table->string('image');
+            $table->longText("report"); //MUDAR POR CAUSA DO UPLOAD PDF (VAI SER STRING (LINK))
+            $table->string('tags');
+
+            //NAO APARECE
+            $table->string('game');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
